fb2997ddc777abf9fd9cd391a54b2c43c84d3d57
nexzanavod <aanavod@gmail.com>
2021-05-26 21:39:02 +0530

    first commit

---
+++ lib/model/movie_response.dart
@@ -0,0 +1,23 @@
+
+import 'package:movieapp/model/movie.dart';
+
+
+
+class MovieResponse{
+  final List<Movie> movies;
+  final String error;
+
+  MovieResponse(this.movies,this.error);
+
+  MovieResponse.fromJson(Map <String, dynamic> json)
+      : movies  = 
+            (json['results'] as List).map((i) => new Movie.fromJson(i)).toList(),
+            error = "";
+
+
+ MovieResponse.withError(String errorValue)
+  // ignore: deprecated_member_use
+  : movies = List(),
+  error = errorValue;
+
+}
