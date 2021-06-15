ff40434023a79bf50f375f87deda0bd5808cb922
ShashiniU <uwanshika1997@gmail.com>
2021-06-16 00:22:47 +0530

    google_login

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:movieapp/authentication/registerPage.dart';
+import 'package:movieapp/screens/home_screen.dart';
 
 void main() {
   runApp(MyApp());
@@ -10,12 +10,12 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
+      debugShowCheckedModeBanner: true,
       title: 'Flutter Demo',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: RegisterPage(),
+      home: HomeScreen(),
     );
   }
 }
